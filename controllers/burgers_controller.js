903fc4ce6c7d501a1fec8402fd832bfd87b3c038
sadiyaj <sadiyajiru@gmail.com>
2018-10-14 23:46:13 -0500

    issue with db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -19,30 +19,33 @@ router.get("/", function(req, res) {
   });
 });
 //Route for Creat/add a new burger
-router.post("/", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   burgers.create([
     "burger_name"], [req.body.burger_name],
-   function() {
+   function(result) {
     // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+
     res.redirect("/");
     });
 });
 //Route for updating the burger by id
-router.put("/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burgers.update(
     {
-      condition: req.body.condition
-    },
-    condition,
-    function() {
-      // Send back the ID of the new burger
-      res.redirect("/");
-    }
-  );
-});
+      devour: req.body.devour
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 module.exports = router;
